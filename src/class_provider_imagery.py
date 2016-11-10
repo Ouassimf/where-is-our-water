38257801226d31747d02b376dd6dd147e9d60358
ouassim <ouassim.fari@gmail.com>
2016-11-10 23:12:58 +0100

    Enhanced downloading of tiff files. Minors corrections

--- src/class_provider_imagery.py
+++ src/class_provider_imagery.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import requests
+import shutil
+
+import sys
+
 import settings as stgs
 from requests.auth import HTTPBasicAuth
 import xml.etree.ElementTree as ETree
@@ -9,12 +13,15 @@ import xml.etree.ElementTree as ETree
 class ProviderImagery:
     """Class for manipulating imagery from Sentinel Sat"""
 
-    def __init__(self, source, uuid, name=None, tiff_files=None):
+    def __init__(self, source, uuid, name=None, acquisition_time=None, gml_coordinates=None, tiff_files=None):
         # source is either "C" or "L" for respectively Copernicus/Landsat
         self.source = source
         # unique identifier from the imagery provider.
         self.uuid = uuid
         self.name = name
+        # array of 2 strings start & stop [ 2016-10-20T05:09:39.423958 , 2016-10-20T05:10:04.423161]
+        self.acquisition_time = acquisition_time
+        self.gml_coordinates = gml_coordinates
         self.tiff_files = tiff_files
 
     def download_quicklook(self):
@@ -34,27 +41,40 @@ class ProviderImagery:
         product_name = self.name + '.SAFE'
         url = stgs.COP_BASE_URL + "/Products('%s')/Nodes('%s')/Nodes('manifest.safe')/$value" % (
             self.uuid, product_name)
-        print(url)
         response = requests.get(url, auth=HTTPBasicAuth(stgs.COP_USERNAME, stgs.COP_PASSWORD))
         filename = "../raw_data/" + self.uuid + "/" + self.uuid + ".xml"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, 'wb') as f:
             f.write(response.content)
+
+        self.populate_information(response.content)
+
         return response.content
 
-    def download_tiff(self, polarization=None):
-        # Download tiff file, polarization is either vv or vh
-        xml = self.download_manifest()
-        product_name = self.name + '.SAFE'
-        tree = ETree.ElementTree(ETree.fromstring(xml))
+    def populate_information(self, xml_string):
+        tree = ETree.ElementTree(ETree.fromstring(xml_string))
         root = tree.getroot()
         self.tiff_files = []
+        self.acquisition_time = []
         for element in root.findall(".//dataObject[@repID='s1Level1MeasurementSchema']"):
             full = element.find('byteStream/fileLocation').attrib['href']
             path, file = os.path.split(full)
             self.tiff_files.append(file)
-            if polarization in full or polarization is None:
-                print("Downloading tiff")
+
+        for element in tree.iter('{http://www.esa.int/safe/sentinel-1.0}startTime'):
+            self.acquisition_time.append(element.text)
+        for element in tree.iter('{http://www.esa.int/safe/sentinel-1.0}stopTime'):
+            self.acquisition_time.append(element.text)
+        for element in tree.iter('{http://www.opengis.net/gml}coordinates'):
+            self.gml_coordinates = element.text
+
+    def download_tiff(self, polarization=None):
+        # Download tiff file, polarization is either vv or vh and optional
+        self.download_manifest()
+        product_name = self.name + '.SAFE'
+        for count, file in enumerate(self.tiff_files):
+            if polarization is None or polarization in file:
+                print("Downloading tiff  " + str(count + 1) + '/' + str(len(self.tiff_files)))
                 print(file)
                 url = stgs.COP_BASE_URL + "/Products('%s')/Nodes('%s')/Nodes('measurement')/Nodes('%s')/$value" % (
                     self.uuid, product_name, file)
@@ -68,7 +88,7 @@ class ProviderImagery:
                         i += 1
                         if chunk:  # filter out keep-alive new chunks
                             f.write(chunk)
-                            print(str(i) + "/" + str(int(size / 1048576)))
+                            self.print_progress(i, int(size / 1048576), "progress", str(int(size / 1048576)) + "mb")
                 return
 
     def delete_raw_data(self):
@@ -76,6 +96,7 @@ class ProviderImagery:
         self.delete_quicklook()
         self.delete_tiff_files()
         self.delete_manifest()
+        shutil.rmtree('../raw_data/' + self.uuid)
 
     def delete_quicklook(self):
         """Delete quicklook of a product"""
@@ -99,3 +120,25 @@ class ProviderImagery:
             print("Name is " + self.name)
         else:
             logging.info("Already a name for " + self.uuid)
+
+    # Print iterations progress
+    @staticmethod
+    def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=50):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            barLength   - Optional  : character length of bar (Int)
+        """
+        format_str = "{0:." + str(decimals) + "f}"
+        percents = format_str.format(100 * (iteration / float(total)))
+        filled_length = int(round(bar_length * iteration / float(total)))
+        bar = '█' * filled_length + '-' * (bar_length - filled_length)
+        sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+        if iteration == total:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
